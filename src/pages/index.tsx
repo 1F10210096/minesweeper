f50881a67baaa698cbdc2e96199fea88fb9b2794
ユーザー名 <koki.1218.dog@icloud.com>
2023-05-24 04:13:29 +0900

    動きます

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,6 +25,8 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
+  const [count, setCount] = useState([[0]]);
+
   const board: number[][] = [
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
@@ -108,13 +110,11 @@ const Home = () => {
       for (let f = -1; f <= 1; f++) {
         const y_e = y + e;
         const x_f = x + f;
-        console.log('a');
         if (board[y_e] === undefined || board[y_e][x_f] === undefined) {
           continue;
         } else if (board[y_e][x_f] !== -1) {
           board2[y_e][x_f] = 0;
         } else if (board[y_e][x_f] === -1) {
-          console.log('b');
           board2[y_e][x_f] = 0;
           board[y_e][x_f] = 0;
           saiki(y_e, x_f);
@@ -131,6 +131,15 @@ const Home = () => {
     }
   }
 
+  for (let h = 0; h < 9; h++) {
+    for (let g = 0; g < 9; g++) {
+      if (userInputs[h][g] === 2) {
+        board[h][g] = 10;
+        board2[h][g] = 0;
+      }
+    }
+  }
+
   //爆発判定
   for (let a = 0; a < 9; a++) {
     for (let b = 0; b < 9; b++) {
@@ -142,6 +151,9 @@ const Home = () => {
             if (board[b][a] === -1) {
               board2[b][a] = 0;
               board[b][a] = 0;
+            }
+            if (bombMap[b][a] === 1 && board[b][a] === 10) {
+              board[b][a] = 11;
             }
           }
         }
@@ -161,45 +173,77 @@ const Home = () => {
   console.log(bombMap);
   console.log(board);
 
-  let count = 0;
+  let flagCount = 0;
+  let bombCount = 0;
+
   for (let h = 0; h < 9; h++) {
     for (let g = 0; g < 9; g++) {
-      if (board[h][g] === -1) {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        count++;
+      if (userInputs[h][g] === 2) {
+        flagCount++;
+      }
+      if (bombMap[h][g] === 1 && userInputs[h][g] === 1) {
+        bombCount++;
       }
     }
   }
-  if (count === 10) {
+
+  if (flagCount + bombCount === 10) {
     alert('クリア');
   }
-  console.log(count);
 
   const onClick = (x: number, y: number, event: React.MouseEvent<HTMLDivElement>) => {
-    const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
+    const isPlaying = userInputs.some((row) => row.some((input) => input === 1));
 
     newuserInputs[y][x] = 1;
 
-    //爆弾を作る
-    if (!isPlaying) {
-      let i = 0;
-      while (i < 10) {
-        const y = Math.floor(Math.random() * 9);
-        const x = Math.floor(Math.random() * 9);
+    event.preventDefault(); // デフォルトの右クリックメニューを無効化
 
-        if (newbombMap[y][x] === 0 && newuserInputs[y][x] !== 1) {
-          newbombMap[y][x] = 1;
-          i++;
-        }
+    if (event.button === 2) {
+      // 右クリックイベントの場合
+      const newInputs: (0 | 1 | 2 | 3)[][] = userInputs.map((row) => row.slice()); // userInputsのコピーを作成
+
+      if (newInputs[y][x] === 0) {
+        // 旗が立っていないセルの場合
+        newInputs[y][x] = 2; // 旗を立てる（2は旗の状態を表す値）
+      } else if (newInputs[y][x] === 2) {
+        // 旗が立っているセルの場合
+        newInputs[y][x] = 0; // 旗を取り消す
       }
-      setBombMap(newbombMap);
+
+      setUserInputs(newInputs); // 新しいユーザーの入力状態を更新
+    } else {
+      //爆弾を作る
+      if (!isPlaying) {
+        let i = 0;
+        while (i < 10) {
+          const y = Math.floor(Math.random() * 9);
+          const x = Math.floor(Math.random() * 9);
+
+          if (newbombMap[y][x] === 0 && newuserInputs[y][x] !== 1) {
+            newbombMap[y][x] = 1;
+            i++;
+          }
+        }
+        setBombMap(newbombMap);
+        // let countN = 0;
+        // const interval_id= undefined;
+        // setInterval(() => {
+        //   countN = countN + 1;
+        //   const newCount: number[][] = [[countN]];
+        //   setCount(newCount);
+        //   if (flagCount + bombCount === 10) {
+        //     clearInterval(interval_id);
+        //   }
+        // }, 1000);
+      }
+      setUserInputs(newuserInputs);
     }
-    setUserInputs(newuserInputs);
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.board1}>
+        <div>{count[0][0]}</div>
         <div className={styles.button} onClick={handleButtonClick}>
           a
         </div>
